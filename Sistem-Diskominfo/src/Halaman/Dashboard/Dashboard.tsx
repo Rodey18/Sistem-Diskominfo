9786df50438b73bbbac1a8ff0fca9a32cac13cc6
hera <herawati21d@student.unhas.ac.id>
2024-05-07 01:36:59 +0800

    update

--- Sistem-Diskominfo/src/Halaman/Dashboard/Dashboard.tsx
+++ Sistem-Diskominfo/src/Halaman/Dashboard/Dashboard.tsx
@@ -1,19 +1,38 @@
-import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import Header from "../../Komponen/Header/Header";
-import ResponsiveDrawer from "../../Komponen/menu/menu";
+// import React from "react";
+// // import { useNavigate } from "react-router-dom";
+// // import Header from "../../Komponen/Header/Header";
+// import ResponsiveDrawer from "../../Komponen/menu/menu";
 
-import "./Dashboard.css";
+// import "./Dashboard.css";
+
+// const Dashboard: React.FC = () => {
+//   // const navigate = useNavigate(); // Jika Anda akan menggunakan useNavigate, nanti bisa diaktifkan
+
+//   return (
+//     <div className="data-stat-container">
+//       {/* <Header /> */}
+//       <ResponsiveDrawer />
+//       <div className="main-content"></div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
+
+import React from "react";
+import { Box } from "@mui/material";
+import PrimarySearchAppBar from "../../Komponen/Header/Header";
+import SideMenuPage from "../../Komponen/menu/menu";
 
 const Dashboard: React.FC = () => {
-  // const navigate = useNavigate(); // Jika Anda akan menggunakan useNavigate, nanti bisa diaktifkan
-
   return (
-    <div className="data-stat-container">
-      {/* <Header /> */}
-      <ResponsiveDrawer />
-      <div className="main-content"></div>
-    </div>
+    <Box>
+      <PrimarySearchAppBar />
+      <Box sx={{ display: "flex" }}>
+        <SideMenuPage />
+        <Box sx={{ flexGrow: 1 }}></Box>
+      </Box>
+    </Box>
   );
 };
 
--- Sistem-Diskominfo/src/Komponen/menu/menu.tsx
+++ Sistem-Diskominfo/src/Komponen/menu/menu.tsx
@@ -186,18 +186,209 @@ const SideMenuPage: React.FC<Props> = ({ window }: Props) => {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            DataStat
-          </Typography>
         </Toolbar>
-        <Typography paragraph>makan sapi</Typography>
-        <Typography paragraph>ayam kawin</Typography>
       </Box>
     </Box>
   );
 };
 
 export default SideMenuPage;
+
+// // components/ResponsiveDrawer.tsx
+// import * as React from "react";
+// import { Link } from "react-router-dom";
+// import {
+//   Box,
+//   CssBaseline,
+//   Drawer,
+//   IconButton,
+//   Toolbar,
+//   Typography,
+//   List,
+//   ListItem,
+//   ListItemButton,
+//   ListItemIcon,
+//   ListItemText,
+//   Divider,
+// } from "@mui/material";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import HomeIcon from "@mui/icons-material/Home";
+// import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+// import PeopleIcon from "@mui/icons-material/People";
+// import LogoutIcon from "@mui/icons-material/Logout";
+
+// const drawerWidth = 240;
+
+// interface Props {
+//   window?: () => Window;
+// }
+
+// const SideMenuPage: React.FC<Props> = ({ window }: Props) => {
+//   const [mobileOpen, setMobileOpen] = React.useState(false);
+//   const [isClosing, setIsClosing] = React.useState(false);
+
+//   const handleDrawerClose = () => {
+//     setIsClosing(true);
+//     setMobileOpen(false);
+//   };
+
+//   const handleDrawerTransitionEnd = () => {
+//     setIsClosing(false);
+//   };
+
+//   const handleDrawerToggle = () => {
+//     if (!isClosing) {
+//       setMobileOpen(!mobileOpen);
+//     }
+//   };
+
+//   const drawer = (
+//     <Box
+//       sx={{
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "space-between",
+//         height: "100%",
+//         overflowX: "hidden",
+//       }}
+//     >
+//       <div>
+//         <Toolbar>
+//           <Typography
+//             variant="h6"
+//             sx={{
+//               color: "white",
+//               backgroundColor: "#000",
+//               padding: "10px",
+//               margin: "20px",
+//               width: "100%",
+//               textAlign: "center",
+//               minWidth: "240px",
+//               m: 0,
+//               ml: 1,
+//               fontSize: "24px",
+//             }}
+//           >
+//             DataStat
+//           </Typography>
+//         </Toolbar>
+//         <List>
+//           <ListItem disablePadding>
+//             <ListItemButton component={Link} to="/dashboard">
+//               <ListItemIcon>
+//                 <HomeIcon />
+//               </ListItemIcon>
+//               <ListItemText primary="Dashboard" />
+//             </ListItemButton>
+//           </ListItem>
+//           <ListItem disablePadding>
+//             <ListItemButton component={Link} to="/uploadfile">
+//               <ListItemIcon>
+//                 <CloudUploadIcon />
+//               </ListItemIcon>
+//               <ListItemText primary="Data" />
+//             </ListItemButton>
+//           </ListItem>
+//           <ListItem disablePadding>
+//             <ListItemButton component={Link} to="/employee">
+//               <ListItemIcon>
+//                 <PeopleIcon />
+//               </ListItemIcon>
+//               <ListItemText primary="Employee" />
+//             </ListItemButton>
+//           </ListItem>
+//         </List>
+//       </div>
+//       <div>
+//         <Divider />
+//         <List>
+//           <ListItem disablePadding>
+//             <ListItemButton component={Link} to="/login">
+//               <ListItemIcon>
+//                 <LogoutIcon />
+//               </ListItemIcon>
+//               <ListItemText primary="Logout" />
+//             </ListItemButton>
+//           </ListItem>
+//         </List>
+//       </div>
+//     </Box>
+//   );
+
+//   const container =
+//     window !== undefined ? () => window().document.body : undefined;
+
+//   return (
+//     <Box sx={{ display: "flex", overflowX: "hidden" }}>
+//       <CssBaseline />
+//       <Box
+//         component="nav"
+//         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+//         aria-label="mailbox folders"
+//       >
+//         <Drawer
+//           container={container}
+//           variant="temporary"
+//           open={mobileOpen}
+//           onTransitionEnd={handleDrawerTransitionEnd}
+//           onClose={handleDrawerClose}
+//           ModalProps={{
+//             keepMounted: true,
+//           }}
+//           sx={{
+//             flexShrink: 0,
+//             display: { xs: "block", sm: "none" },
+//             "& .MuiDrawer-paper": {
+//               boxSizing: "border-box",
+//               width: drawerWidth,
+//               overflowX: "hidden",
+//             },
+//           }}
+//         >
+//           {drawer}
+//         </Drawer>
+//         <Drawer
+//           variant="permanent"
+//           sx={{
+//             flexShrink: 0,
+//             display: { xs: "none", sm: "block" },
+//             "& .MuiDrawer-paper": {
+//               boxSizing: "border-box",
+//               width: drawerWidth,
+//               overflowX: "hidden",
+//             },
+//           }}
+//           open
+//         >
+//           {drawer}
+//         </Drawer>
+//       </Box>
+//       <Box
+//         component="main"
+//         sx={{
+//           flexGrow: 1,
+//           p: 3,
+//           width: { sm: `calc(100% - ${drawerWidth}px)` },
+//           overflowX: "hidden",
+//         }}
+//       >
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             edge="start"
+//             onClick={handleDrawerToggle}
+//             sx={{ mr: 2, display: { sm: "none" } }}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//         </Toolbar>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default SideMenuPage;
 
 // import React from "react";
 // import {
